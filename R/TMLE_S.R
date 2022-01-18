64aa95ed6e3a24aa310c14ff4b6c80c14dfc43b1
jenniferxu1101 <sxu42@jhmi.edu>
2022-01-18 13:17:20 -0500

    replace dlong parameter

--- R/TMLE_S.R
+++ R/TMLE_S.R
@@ -9,26 +9,34 @@
 #' @param tau Time of interest. Can be a vector (multiple time of interest)
 #' @return A data frame with three columns: SurvProb1, SurvProb0, SEprobDiff
 
-estimateTMLEprob <- function(dlong, survHaz, cenHaz, treatProb, tau){
+estimateTMLEprob <- function(treatment, eventObserved, time, survHaz, cenHaz, treatProb, tau){
 
   ## container
   SurvProb1_result <- SurvProb0_result <- SEprobDiff_result <- rep(0, length=length(tau))
 
   ## calculate censoring probability that doesn't need iterative updates
-  CenProb1List <- tapply(1 - cenHaz$Haz1, dlong$id, cumprod, simplify = FALSE)
-  CenProb0List <- tapply(1 - cenHaz$Haz0, dlong$id, cumprod, simplify = FALSE)
+  CenProb1List <- tapply(1 - cenHaz$Haz1, cenHaz$ID, cumprod, simplify = FALSE)
+  CenProb0List <- tapply(1 - cenHaz$Haz0, cenHaz$ID, cumprod, simplify = FALSE)
 
   CenProb1 <- unlist(CenProb1List, use.names = FALSE)
   CenProb0 <- unlist(CenProb0List, use.names = FALSE)
 
   rm(list=c("CenProb1List", "CenProb0List", "cenHaz"))
 
+  ## parameter
+  maxTime <- dim(survHaz)[1]/length(unique(survHaz$ID))
+  n <- length(unique(survHaz$ID))
+
+  ## dlong
+  dlong <- transformData(dwide=data.frame(eventObserved=eventObserved, time=time), freqTime=1)
+  dlong <- dlong[which(dlong$t <= maxTime),]
+
   for (TimePoint in tau){
 
     ## initial SurvHaz
     SurvHaz1 <- survHaz$Haz1
     SurvHaz0 <- survHaz$Haz0
-    SurvHaz_obs  <- dlong$treatment*SurvHaz1 + (1-dlong$treatment)*SurvHaz0
+    SurvHaz_obs  <- treatment[survHaz$ID]*SurvHaz1 + (1-treatment[survHaz$ID])*SurvHaz0
 
     ## parameter
     ind <- (dlong$t <= TimePoint)
@@ -38,8 +46,8 @@ estimateTMLEprob <- function(dlong, survHaz, cenHaz, treatProb, tau){
     ## iterate
     while((!converged) && iter <= 20){
 
-      SurvProb1List <- tapply(1 - SurvHaz1, dlong$id, cumprod, simplify = FALSE)
-      SurvProb0List <- tapply(1 - SurvHaz0, dlong$id, cumprod, simplify = FALSE)
+      SurvProb1List <- tapply(1 - SurvHaz1, SurvHaz$ID, cumprod, simplify = FALSE)
+      SurvProb0List <- tapply(1 - SurvHaz0, SurvHaz$ID, cumprod, simplify = FALSE)
 
       SurvProb1 <- unlist(SurvProb1List, use.names = FALSE)
       SurvProb0 <- unlist(SurvProb0List, use.names = FALSE)
@@ -47,9 +55,9 @@ estimateTMLEprob <- function(dlong, survHaz, cenHaz, treatProb, tau){
       rm(list=c("SurvProb1List", "SurvProb0List"))
 
       ## clever covariate for updating survival hazard
-      H1 <- - (ind * rep(SurvProb1[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb1 * treatProb[dlong$id] * CenProb1)
-      H0 <- - (ind * rep(SurvProb0[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb0 * (1-treatProb[dlong$id]) * CenProb0)
-      H <- dlong$treatment * H1 + (1-dlong$treatment) * H0
+      H1 <- - (ind * rep(SurvProb1[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb1 * treatProb[SurvHaz$ID] * CenProb1)
+      H0 <- - (ind * rep(SurvProb0[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb0 * (1-treatProb[SurvHaz$ID]) * CenProb0)
+      H <- treatment[survHaz$ID] * H1 + (1-treatment[survHaz$ID]) * H0
 
       ## update for survival hazard
       eps   <- coef(glm2::glm2(Lt ~ 0 + offset(qlogis(SurvHaz_obs)) + H,
@@ -61,7 +69,7 @@ estimateTMLEprob <- function(dlong, survHaz, cenHaz, treatProb, tau){
       ## update values
       SurvHaz1 <- plogis(qlogis(SurvHaz1) + eps * H1)
       SurvHaz0 <- plogis(qlogis(SurvHaz0) + eps * H0)
-      SurvHaz_obs  <- dlong$treatment * SurvHaz1  + (1 - dlong$treatment) * SurvHaz0
+      SurvHaz_obs  <- treatment[survHaz$ID] * SurvHaz1  + (1 - treatment[survHaz$ID]) * SurvHaz0
 
       iter <-  iter + 1
       converged <- (abs(eps) <= 1e-3/length(unique(dlong$id))^(0.6))
@@ -72,17 +80,17 @@ estimateTMLEprob <- function(dlong, survHaz, cenHaz, treatProb, tau){
     }
 
     ## result for time tau
-    SurvProb1List <- tapply(1 - SurvHaz1, dlong$id, cumprod, simplify = FALSE)
-    SurvProb0List <- tapply(1 - SurvHaz0, dlong$id, cumprod, simplify = FALSE)
+    SurvProb1List <- tapply(1 - SurvHaz1, survHaz$ID, cumprod, simplify = FALSE)
+    SurvProb0List <- tapply(1 - SurvHaz0, survHaz$ID, cumprod, simplify = FALSE)
 
     SurvProb1 <- unlist(SurvProb1List, use.names = FALSE)
     SurvProb0 <- unlist(SurvProb0List, use.names = FALSE)
 
     rm(list=c("SurvProb1List", "SurvProb0List", "SurvHaz1", "SurvHaz0"))
 
-    H1 <- - (ind * rep(SurvProb1[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb1 * treatProb[dlong$id] * CenProb1)
-    H0 <- - (ind * rep(SurvProb0[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb0 * (1-treatProb[dlong$id]) * CenProb0)
-    DT <- with(dlong, tapply(It * (treatment * H1 - (1 - treatment) * H0) * (Lt - SurvHaz_obs), id, sum))
+    H1 <- - (ind * rep(SurvProb1[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb1 * treatProb[SurvHaz$ID] * CenProb1)
+    H0 <- - (ind * rep(SurvProb0[which(dlong$t == TimePoint)], each=max(dlong$t))) / bound(SurvProb0 * (1-treatProb[SurvHaz$ID]) * CenProb0)
+    DT <- tapply(dlong$It * (treatment[SurvHaz$ID] * H1 - (1 - treatment[SurvHaz$ID]) * H0) * (dlong$Lt - SurvHaz_obs), SurvHaz$ID, sum)
 
     rm(list=c("H1", "H0", "ind"))
 
@@ -97,7 +105,7 @@ estimateTMLEprob <- function(dlong, survHaz, cenHaz, treatProb, tau){
     SurvProb0_mean <- mean(DW0)
     ## standard error of S(1, tau)-S(0, tau)
     D <- DT + DW1 - DW0
-    sdn <- sqrt(var(D) / length(unique(dlong$id)))
+    sdn <- sqrt(var(D) / n)
 
     rm(list=c("DT", "DW1", "DW0", "D"))
 
