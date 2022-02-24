4675c93cfb7765bf5d291b80384d1154abebb55f
jenniferxu1101 <sxu42@jhmi.edu>
2022-02-24 10:50:48 -0500

    Incorporate non-uniform time interval

--- R/IPW.R
+++ R/IPW.R
@@ -6,7 +6,9 @@
 #' @param tau Time of interest. Can be a vector (multiple time of interest)
 #' @return A data frame with three columns: estimand1, estimand0, SE
 
-estimateIPW <- function(treatment, eventObserved, time, cenHaz, treatProb, tau, estimand, printTau){
+estimateIPW <- function(treatment, eventObserved, time,
+                        cenHaz, treatProb,
+                        tau, timeIntMidPoint, estimand, printTau){
 
   ## container
   estimand1_result <- estimand0_result <- SE_result <- rep(0, length=length(tau))
@@ -17,23 +19,18 @@ estimateIPW <- function(treatment, eventObserved, time, cenHaz, treatProb, tau, 
   ID <- rep(1:n, each=maxTime)
 
   ## dlong
-  dlong <- transformData(dwide=data.frame(eventObserved=eventObserved, time=time, treat=treatment), freqTime=1, type="survival")
+  dlong <- transformData(dwide=data.frame(eventObserved=eventObserved, time=time, treat=treatment), timeIntMidPoint=timeIntMidPoint, type="survival")
   rownames(dlong) <- NULL
-  dlong <- dlong[which(dlong$t <= maxTime), c("Lt", "It", "t", "treat")]
 
   ## Estimate survival hazards
-  survHazFit <- lm(Lt ~ as.factor(t) * as.factor(treat), subset = It == 1, data = dlong)
-  survHaz1 <- predict(survHazFit, newdata = data.frame(t=1:maxTime, treat=rep(1, maxTime)), type = 'response')
-  survHaz0 <- predict(survHazFit, newdata = data.frame(t=1:maxTime, treat=rep(0, maxTime)), type = 'response')
-
-  rm(list=c("survHazFit"))
-
-  survHaz1 <- rep(survHaz1, n)
-  survHaz0 <- rep(survHaz0, n)
+  survHaz <- coef_lm(dlong=dlong, timeIntMidPoint=timeIntMidPoint, n=n, printIter=TRUE)
+  survHaz1 <- survHaz$Haz1
+  survHaz0 <- survHaz$Haz0
+  rm(list=c("survHaz"))
 
   ## calculate survival and censoring probability
-  SurvProb1List <- tapply(1 - survHaz1, ID, cumprod, simplify = FALSE)
-  SurvProb0List <- tapply(1 - survHaz0, ID, cumprod, simplify = FALSE)
+  SurvProb1List <- rep(cumprod(1 - survHaz1), n)
+  SurvProb0List <- rep(cumprod(1 - survHaz0), n)
 
   SurvProb1 <- unlist(SurvProb1List, use.names = FALSE)
   SurvProb0 <- unlist(SurvProb0List, use.names = FALSE)
@@ -65,7 +62,7 @@ estimateIPW <- function(treatment, eventObserved, time, cenHaz, treatProb, tau, 
   Z0weight[which(Z0weight >= quantile(Z0weight, probs = 0.95))] <- quantile(Z0weight, probs = 0.95)
 
 
-  for (TimePoint in tau){
+  for (TimePoint in 1:length(tau)){
 
     if(estimand=="rmst"){
       if(TimePoint == 1){next}
@@ -73,9 +70,9 @@ estimateIPW <- function(treatment, eventObserved, time, cenHaz, treatProb, tau, 
 
     ## parameter
     if(estimand=="rmst"){
-      ind <- (dlong$t <= (TimePoint -1))
+      ind <- (dlong$t <= tau[TimePoint -1])
     }else if(estimand=="risk"){
-      ind <- (dlong$t <= TimePoint)
+      ind <- (dlong$t <= tau[TimePoint])
     }
 
     ## IPW
@@ -103,8 +100,8 @@ estimateIPW <- function(treatment, eventObserved, time, cenHaz, treatProb, tau, 
 
     ## variance
     if(estimand=="risk"){
-      H1 <- - (ind * rep(SurvProb1[which(dlong$t == TimePoint)], each=max(dlong$t))) * weightH1
-      H0 <- - (ind * rep(SurvProb0[which(dlong$t == TimePoint)], each=max(dlong$t))) * weightH0
+      H1 <- - (ind * rep(SurvProb1[which(dlong$t == tau[TimePoint])], each=maxTime)) * weightH1
+      H0 <- - (ind * rep(SurvProb0[which(dlong$t == tau[TimePoint])], each=maxTime)) * weightH0
     }else if(estimand=="rmst"){
       cumProb1TillTimePoint <- unlist(tapply(ind * SurvProb1, ID, function(x){rev(cumsum(rev(x)))}), use.names = FALSE)
       H1 <- - cumProb1TillTimePoint * weightH1
@@ -120,8 +117,8 @@ estimateIPW <- function(treatment, eventObserved, time, cenHaz, treatProb, tau, 
     rm(list=c("H1", "H0"))
 
     if(estimand=="risk"){
-      DW1 <- SurvProb1[which(dlong$t == TimePoint)]
-      DW0 <- SurvProb0[which(dlong$t == TimePoint)]
+      DW1 <- SurvProb1[which(dlong$t == tau[TimePoint])]
+      DW0 <- SurvProb0[which(dlong$t == tau[TimePoint])]
     }else if(estimand=="rmst"){
       DW1 <- tapply(ind * SurvProb1, ID, sum)
       DW0 <- tapply(ind * SurvProb0, ID, sum)
