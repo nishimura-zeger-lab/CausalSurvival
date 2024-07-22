e80679d0185fd11fb1634014d3a22fbacb899d11
jenniferxu1101 <sxu42@jhmi.edu>
2024-07-22 00:12:41 -0400

    First commit

---
+++ R/matrixProduct.R
@@ -0,0 +1,37 @@
+#' Calculate Xv
+#'
+#' @param temporal_effect vector
+#' @param X_baseline sparse Matrix of class "dgTMatrix"
+#' @export result A vector
+pooled_design_matvec <- function(temporal_effect, X_baseline){
+    result <- rep(0, dim(X_baseline)[1])
+    n_nonzero <- length(X_baseline@x)
+    for (k in 1:n_nonzero) {
+      result[X_baseline@i[k] + 1] <- result[X_baseline@i[k] + 1] + X_baseline@x[k] * temporal_effect[X_baseline@j[k] + 1]
+    }
+    return(result)
+}
+
+
+
+#' Calculate X^T diag(W) X
+#'
+#' @param X_baseline sparse Matrix of class "dgTMatrix"
+#' @param weight vector
+#' @export result A matrix
+pooled_design_information <- function(X_baseline, weight){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
