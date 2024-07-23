5c8e7e89f475099d6739975cb0017ba6833eaa89
jenniferxu1101 <sxu42@jhmi.edu>
2024-07-22 22:36:45 -0400

    First half of pooled_design_information

--- R/PooledLogistic.R
+++ R/PooledLogistic.R
@@ -32,19 +32,58 @@ pooled_design_matvec <- function(X_baseline, temporal_effect, eventObserved, tim
     result_temporal <- result_temporal + i*t(result_temporal[indx_subset[i], ])%*%time_reorder[indx_subset[i]] ## could add functions to i
   }
   ## result
-  return(list(vec_X=result_X, vec_temporal=result_temporal))
+  return(c(result_X, result_temporal))
 }
 
 
 
-#' Calculate X^T diag(W) X for pooled logistic regression
+#' Calculate X^T diag(D) X for pooled logistic regression
 #'
-#' @param X_baseline sparse Matrix of class "dgTMatrix"
-#' @param weight vector
+#' @param X_baseline Baseline variables that won't interact with time in regression,
+#'                  sparse matrix of class "dgTMatrix"
+#' @param temporal_effect Baseline variables that will interact with time in regression,
+#'                        sparse matrix of class "dgTMatrix" or matrix
+#' @param eventObserved Binary outcome variable
+#' @param time Observed time
+#' @param id Subject id
 #' @export result A matrix
-pooled_design_information <- function(temporal_effect, X_baseline, weight, eventObserved, time){
+pooled_design_information <- function(X_baseline, temporal_effect, eventObserved, time, id){
+  ## index for decreasing survival time
+  indx <- order(time, decreasing = TRUE)
+  ## order observed data into decreasing survival time
+  id_reorder <- id[indx]
+  eventObserved_reorder <- eventObserved[indx]
+  time_reorder <- time[indx]
+  X_baseline_reorder <- X_baseline[indx, ]
+  temporal_effect_reorder <- temporal_effect[indx, ]
+  ## Add intercept term to X_baseline_reorder and temporal_effect_reorder
+  X_baseline_reorder <- cbind(rep(1, dim(X_baseline_reorder)[1]), X_baseline_reorder)
+  temporal_effect_reorder <- cbind(rep(1, dim(X_baseline_reorder)[1]), temporal_effect_reorder)
+  ## subset index for each time point
+  K <- max(time_reorder)
+  indx_subset <- sapply(1:K, function(x) sum(time_reorder>=x), USE.NAMES = FALSE)
+  ## calculate weights in IRLS
+
+  ## container
+
+  ## loop over each time point
+
+  ## result
+
 
 }
+
+
+
+#' Coefficients for pooled logistic regression (via iterative reweighted least square)
+
+
+
+
+
+
+
+
 
 
 
