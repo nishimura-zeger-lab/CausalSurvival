dd9fa8f7a1b48b432eaca5f6d4d2c527c5ccc0e0
jenniferxu1101 <sxu42@jhmi.edu>
2024-07-28 12:08:09 -0400

    Reconstruct iterations

--- R/PooledLogistic.R
+++ R/PooledLogistic.R
@@ -1,4 +1,4 @@
-#' Calculate X^T y, X^T mu, D and X^T diag(D) X for pooled logistic regression
+#' Coefficients for pooled logistic regression (via iterative reweighted least square)
 #'
 #' @param X_baseline Baseline variables that won't interact with time in regression,
 #'                  sparse matrix of class "dgTMatrix"
@@ -8,8 +8,8 @@
 #' @param time Observed time
 #' @param id Subject id
 #' @param estimate_hazard "survival" or "censoring"
-#' @export result A vector
-pooled_design <- function(X_baseline, temporal_effect, beta, eventObserved, time, id, estimate_hazard){
+#' @export
+coef_pooled <- function(X_baseline, temporal_effect, eventObserved, time, id, estimate_hazard){
 
   ## index for decreasing survival time
   indx <- order(time, decreasing = TRUE)
@@ -30,13 +30,100 @@ pooled_design <- function(X_baseline, temporal_effect, beta, eventObserved, time
   K <- max(time_reorder)
   if (estimate_hazard == "survival"){
     indx_subset <- sapply(1:K, function(x) sum(time_reorder>=x), USE.NAMES = FALSE)
+    ## create outcome variable
+    y <- eventObserved_reorder * (time_reorder == i)
   }else if(estimate_hazard == "censoring"){
     indx_subset <- sapply(1:K, function(x) sum((time_reorder>x)*eventObserved_reorder+(time_reorder>=x)*(1-eventObserved_reorder)), USE.NAMES = FALSE)
+    ## create outcome variable
+    y <- (1 - eventObserved_reorder) * (time_reorder == i)
   }
+
+  ## initial value
+  crit <- TRUE
+  iter <- 1
+  beta <- rep(0, length=dim(temporal_effect_reorder)[2]+dim(X_baseline_reorder)[2])
+
+  ## calculate X^T y
+  design_matvec_Xy <- pooled_design_matvec(X_baseline_reorder=X_baseline_reorder,
+                                           temporal_effect_reorder=temporal_effect_reorder,
+                                           y=y, indx_subset=indx_subset)
+
+  ## iterate until converge
+  while(crit && iter <= 20){
+
+    ## calculate iterative components
+    comp <- pooled_design_iter(X_baseline_reorder=X_baseline_reorder,
+                               temporal_effect_reorder=temporal_effect_reorder,
+                               y=y, beta=beta, indx_subset=indx_subset)
+
+    ## beta_new
+    beta_new <- solve(design_information) %*% (comp$design_information %*% beta + design_matvec_Xy - comp$design_matvec_Xmu)
+
+    ## stopping rule
+    iter <-  iter + 1
+    crit <- max(abs(beta_new-beta)/abs(beta)) > 1e-5
+
+    ## update value
+    beta <- beta_new
+
+    ## clear workspace
+    rm(list=c("comp","beta_new"))
+  }
+
+  ## result
+  return(beta)
+}
+
+
+
+#' Calculate X^T y for pooled logistic regression (this part doesn't need to iterate)
+#'
+#' @param X_baseline_reorder Baseline variables that won't interact with time in regression,
+#'                  sparse matrix of class "dgTMatrix",
+#'                  reorder observations into decreasing survival time,
+#'                  intercept included
+#' @param temporal_effect_reorder Baseline variables that will interact with time in regression,
+#'                        sparse matrix of class "dgTMatrix" or matrix,
+#'                        reorder observations into decreasing survival time,
+#'                        intercept included
+#' @param y Outcome variable in the pooled logistic regression
+#' @param indx_subset Subset index for each time point
+#' @export result
+pooled_design_matvec <- function(X_baseline_reorder, temporal_effect_reorder, y, indx_subset){
 
   ## container
   result_Xy <- rep(0, length=dim(X_baseline_reorder)[2])
   result_temporaly <- rep(0, length=dim(temporal_effect_reorder)[2])
+
+  ## loop over each time point
+  for (i in 1:K){
+  ## X^T y
+  result_Xy <- result_Xy + t(X_baseline_reorder[indx_subset[i], ])%*%y[indx_subset[i]]
+  result_temporaly <- result_temporaly + i*t(temporal_effect_reorder[indx_subset[i], ])%*%y[indx_subset[i]] ## could add functions to i
+  }
+  ## result
+  return(c(result_Xy, result_temporaly))
+}
+
+
+
+#' Calculate X^T y, X^T mu, D and X^T diag(D) X for pooled logistic regression
+#'
+#' @param X_baseline_reorder Baseline variables that won't interact with time in regression,
+#'                  sparse matrix of class "dgTMatrix",
+#'                  reorder observations into decreasing survival time,
+#'                  intercept included
+#' @param temporal_effect_reorder Baseline variables that will interact with time in regression,
+#'                        sparse matrix of class "dgTMatrix" or matrix,
+#'                        reorder observations into decreasing survival time,
+#'                        intercept included
+#' @param y Outcome variable in the pooled logistic regression
+#' @param beta Current iteration of coefficient value
+#' @param indx_subset Subset index for each time point
+#' @export result A list
+pooled_design_iter <- function(X_baseline_reorder, temporal_effect_reorder, y, beta, indx_subset){
+
+  ## container
   result_info <- matrix(0, nrow=(dim(temporal_effect_reorder)[2]+dim(X_baseline_reorder)[2]),
                         ncol=(dim(temporal_effect_reorder)[2]+dim(X_baseline_reorder)[2]))
   result_mu <- c()
@@ -45,15 +132,6 @@ pooled_design <- function(X_baseline, temporal_effect, beta, eventObserved, time
 
   ## loop over each time point
   for (i in 1:K){
-    ## outcome variable
-    if (estimate_hazard == "survival"){
-      y <- eventObserved_reorder * (time_reorder == i)
-    }else if(estimate_hazard == "censoring"){
-      y <- (1 - eventObserved_reorder) * (time_reorder == i)
-    }
-    ## X^T y
-    result_Xy <- result_Xy + t(X_baseline_reorder[indx_subset[i], ])%*%y[indx_subset[i]]
-    result_temporaly <- result_temporaly + i*t(temporal_effect_reorder[indx_subset[i], ])%*%y[indx_subset[i]] ## could add functions to i
     ## mu
     temp_mu <- 1/(1+exp(-X_baseline_reorder[indx_subset[i], ]%*%beta[1:dim(X_baseline_reorder)[2]]-i*temporal_effect_reorder[indx_subset[i], ]%*%beta[(dim(X_baseline_reorder)[2]+1):length(beta)]))
     result_mu <- c(result_mu, temp_mu)
@@ -70,39 +148,11 @@ pooled_design <- function(X_baseline, temporal_effect, beta, eventObserved, time
     rm(list=c("temp_mu", "temp_X", "temp_temporal", "temp_Xtemporal"))
   }
   ## result
-  return(list(design_matvec=c(result_Xy, result_temporaly),
-              design_matvec2=c(result_Xmu, result_temporalmu),
-              design_information=result_info, mu=result_mu,
-              indx_reorder=indx, indx_subset=indx_subset))
+  return(list(design_matvec_Xmu=c(result_Xmu, result_temporalmu),
+              design_information=result_info))
 }
 
 
-
-#' Coefficients for pooled logistic regression (via iterative reweighted least square)
-#'
-#'
-coef_pooled <- function(design_matvec, design_matvec2, design_information, mu, eventObserved, indx_reorder, indx_subset){
-
-  ## order observed data into decreasing survival time
-  eventObserved_reorder <- eventObserved[indx_reorder]
-
-  ## iterate until converge
-  crit <- TRUE
-  iter <- 1
-  beta <- rep(0, length=dim(design_information)[1])
-  while(crit && iter <= 20){
-    beta_new <- solve(design_information) %*% (design_information %*% beta + design_matvec - design_matvec2)
-
-    iter <-  iter + 1
-    crit <- abs(sum(beta_new-beta)/sum(beta)) > 1e-5
-
-    ## update value
-    beta <- beta_new
-  }
-
-  ## result
-  return(beta)
-}
 
 
 
