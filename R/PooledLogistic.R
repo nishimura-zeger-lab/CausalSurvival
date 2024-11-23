cb089d5d5cfd49bb8b64a07f7d4a5848a51e6546
jenniferxu1101 <sxu42@jhmi.edu>
2024-11-22 22:18:05 -0500

    tune algorithm

--- R/PooledLogistic.R
+++ R/PooledLogistic.R
@@ -434,12 +434,10 @@ coef_ridge <- function(X_baseline, is.temporal, temporal_effect,
 
 marginalLogLik <- function(betaMAP, lambda, p, logLik, fisherInfo){
 
+  sigma <- 1/sqrt(2*lambda)
+
   ## marginal log likelihood
-  # logLikelihood <- logLik + p*log(2*lambda)/2 - sum(betaMAP[2:(p+1)]^2)*lambda - determinant(as.matrix(fisherInfo)[2:(p+1), 2:(p+1)])$modulus/2
-
-  logLikelihood <- logLik + p*log(2*lambda)/2 + (length(betaMAP)-p)*log(sqrt(2*pi)) - sum(betaMAP[2:(p+1)]^2)*lambda - determinant(as.matrix(fisherInfo))$modulus/2
-
-  # logLikelihood <- logLik - sum(betaMAP[2:(p+1)]^2)*lambda + length(betaMAP)/2*log(2*pi) - determinant(as.matrix(fisherInfo))$modulus/2
+  logLikelihood <- logLik - p * log(sqrt(2*pi)*sigma) - sum(betaMAP[2:(p+1)]^2)/(2*sigma^2) + length(betaMAP) * log(sqrt(2*pi)) + determinant(solve(as.matrix(fisherInfo)))$modulus/2
 
   ## result
   return(logLikelihood)
