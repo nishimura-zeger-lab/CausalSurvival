5fa7fa96093b7f7c7e68026a9e9a45303757587a
Aki Nishimura <aki.nishimura@jhu.edu>
2024-12-07 18:04:46 -0500

    Clean up Rcpp linalg code

--- src/SparseLinalg.cpp
+++ src/SparseLinalg.cpp
@@ -5,18 +5,19 @@ using Eigen::Map; // 'Map' (i.e. reference without making copies) R matrices
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using Eigen::SparseMatrix;
-typedef Eigen::DiagonalMatrix<double, Eigen::Dynamic> EigenDiagonalMatrix;
+typedef Eigen::DiagonalMatrix<double, Eigen::Dynamic> DiagonalMatrix;
+typedef Eigen::SparseMatrix<double, Eigen::RowMajor> CsrMatrix;
 typedef Map<SparseMatrix<double, Eigen::RowMajor>> MappedCsr;
 
 
 // [[Rcpp::export]]
 MatrixXd computeSubsetInformationMatrix(
-  const MappedCsr X, const Map<VectorXd> weight, int subsetSize
+  const Map<CsrMatrix> X, const Map<VectorXd> weight, int subsetSize
 ) {
   int nPred = X.cols();
   MatrixXd subsetInfoMat = MatrixXd::Zero(nPred, nPred);
-  EigenDiagonalMatrix subsetWeightMat = weight.head(subsetSize).asDiagonal();
-  SparseMatrix<double, Eigen::RowMajor> subsetX = X.topRows(subsetSize);
+  DiagonalMatrix subsetWeightMat = weight.head(subsetSize).asDiagonal();
+  CsrMatrix subsetX = X.topRows(subsetSize);
   subsetInfoMat = subsetX.transpose() * subsetWeightMat * subsetX;
   return subsetInfoMat;
 }
